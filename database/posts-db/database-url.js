11f0ae18d3eb453da90291ea0917bbc294d1a6f3
rost <rostislav.chanov@gmail.com>
2022-12-06 21:49:16 +0300

    first commit

---
+++ database/posts-db/database-url.js
@@ -0,0 +1,6 @@
+const username = "rost9911";
+const password = "Neymarisc0nt!;";
+
+const DB_URL = "mongodb+srv://" + username + ":" + password + "@cluster0.ehy1pmj.mongodb.net/?retryWrites=true&w=majority";
+
+export default DB_URL;
